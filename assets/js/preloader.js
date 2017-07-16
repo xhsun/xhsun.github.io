fd8f44e17bdc8ac6adaaddfd2a571aa0ed1d8fbe
xhsun <xhsun@ucalgary.ca>
2017-07-16 12:20:07 -0600

    fixed problem where reloading take a really long time for page with image/video from other site(s)

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,11 +1,15 @@
 if (window.addEventListener) {// For all major browsers, except IE 8 and earlier
 	window.addEventListener("load", function(){
 		$("#preload").fadeOut("slow");
-		$('.load-later').show();
+		$('noscript.load-later').replaceWith(function(){
+			return this.textContent || this.innerText;
+		});
 	});
 } else if (window.attachEvent) {// For IE 8 and earlier versions
 	window.attachEvent("load", function(){
 		$("#preload").fadeOut("slow");
-		$('.load-later').show();
+		$('noscript.load-later').replaceWith(function(){
+			return this.textContent || this.innerText;
+		});
 	});
 }
