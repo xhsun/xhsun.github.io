62074363e62947afd91af63e2f17361c4d1d1e11
xhsun <xhsun@ucalgary.ca>
2017-07-16 11:59:37 -0600

    fixed a problem where loading take longer than needed if page contain image/video from other site(s)

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,5 +1,11 @@
 if (window.addEventListener) {// For all major browsers, except IE 8 and earlier
-	window.addEventListener("load", function(){$("#preload").fadeOut("slow");});
+	window.addEventListener("load", function(){
+		$("#preload").fadeOut("slow");
+		$('.load-later').show();
+	});
 } else if (window.attachEvent) {// For IE 8 and earlier versions
-	window.attachEvent("load", function(){$("#preload").fadeOut("slow");});
+	window.attachEvent("load", function(){
+		$("#preload").fadeOut("slow");
+		$('.load-later').show();
+	});
 }
